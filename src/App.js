ea49ac40768bc06d8bca46717534662b66041b51
Steven Bryant <stevelad_83@yahoo.com>
2023-01-23 18:49:44 -0500

    displaying book by id

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import BookList from './components/book/BookList';
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
+import BookDetail from './components/book/BookDetail.js';
 
 function App() {
   // TODO: Add routes to books & views
@@ -9,6 +10,7 @@ function App() {
       <main className="container">
         <h1>Library Catalog</h1>
         <Switch>
+          <Route exact path="/books/:id" component={BookDetail} />
           <Route path="/books" component={BookList} />
         </Switch>
       </main>
--- src/hooks/useBookDetail.js
+++ src/hooks/useBookDetail.js
@@ -1,13 +1,14 @@
 import { useEffect, useState } from 'react';
 import { getBookById } from '../services/books';
 
-export function useBookDetail({ id }) {
+export function useBookDetail(id) {
   const [book, setBook] = useState();
   const [error, setError] = useState('');
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
+      console.log('id', id);
       try {
         const data = await getBookById(id);
         setBook(data);
