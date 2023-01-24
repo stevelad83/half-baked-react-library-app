1dc60b3068c3baf49a2fd2a3789625323a8df2dd
Steven Bryant <stevelad_83@yahoo.com>
2023-01-23 19:56:24 -0500

    added links to and from bookdetails

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import BookList from './components/book/BookList';
 import './App.css';
-import { Route, Switch } from 'react-router-dom';
+import { Link, Route, Switch } from 'react-router-dom';
 import BookDetail from './components/book/BookDetail.js';
 
 function App() {
@@ -9,6 +9,7 @@ function App() {
     <>
       <main className="container">
         <h1>Library Catalog</h1>
+        <Link to="/books">See the books!</Link>
         <Switch>
           <Route exact path="/books/:id" component={BookDetail} />
           <Route path="/books" component={BookList} />
--- src/components/book/BookList.js
+++ src/components/book/BookList.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import Book from '../../components/book/Book';
 import { useBooks } from '../../hooks/useBooks';
 
@@ -14,7 +15,9 @@ function BookList() {
     <ul className="book-list" aria-label="book list">
       {books.map((book) => (
         <li key={book.book_id}>
-          <Book book={book} />
+          <Link to={`/books/${book.book_id}`}>
+            <Book book={book} />
+          </Link>
         </li>
       ))}
     </ul>
